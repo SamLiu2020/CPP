c50fa0de641f290192ae6c64c5c4df33babd4910
Sam Liu <liu.sam2020@gmail.com>
2020-07-19 11:23:08 -0400

    Added stuff to onstrike.cpp

--- Fundamentals/3.2 OnStrike.cpp
+++ Fundamentals/3.2 OnStrike.cpp
@@ -6,16 +6,17 @@ int main() {
     cin >> x; // Input the Grade
     int y = 20;
 
-    // if ((x >= 1) && (x < 7)) {
-    //     cout << "$20 " << endl;
-    // } else if ((x >= 7) && (x <= 12)) {
-    //     cout << "$30 " << endl;
-    // }
 
-    if ((x >= 1) && (x < 7)) {
+    if ((x >= 1) && (x < 7)) {   // If and else
+        cout << "$20 " << endl;
+    } else if ((x >= 7) && (x <= 12)) {
+        cout << "$30 " << endl;
+    }
+
+
+    if ((x >= 1) && (x < 7)) {   // Two ifs
         cout << y << endl;
-    } 
-    if ((x >= 7) && (x <= 12)) {
+    } if ((x >= 7) && (x <= 12)) {
         cout << y + 10 << endl;
     }
 
