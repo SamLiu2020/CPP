a025a24a8e49ec067d81a599dc0588c2f305da37
Sam Liu <liu.sam2020@gmail.com>
2020-07-19 11:19:28 -0400

    Onstrike question

---
+++ Fundamentals/3.2 OnStrike.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+
+    int x; // Grade Number
+    cin >> x; // Input the Grade
+    int y = 20;
+
+    // if ((x >= 1) && (x < 7)) {
+    //     cout << "$20 " << endl;
+    // } else if ((x >= 7) && (x <= 12)) {
+    //     cout << "$30 " << endl;
+    // }
+
+    if ((x >= 1) && (x < 7)) {
+        cout << y << endl;
+    } 
+    if ((x >= 7) && (x <= 12)) {
+        cout << y + 10 << endl;
+    }
+
+    return 0;
+}
