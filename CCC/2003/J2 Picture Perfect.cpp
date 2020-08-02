9db9f67206e8bbda9196e4c0ca4b79d926f3dd40
Sam Liu <liu.sam2020@gmail.com>
2020-08-02 17:28:15 -0400

    Added somestuff

---
+++ CCC/2003/J2 Picture Perfect.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int num;
+    cin >> num;
+    cout << "Minimum perimeter is " << num * 2 << " with dimensions " << endl;
+    return 0;
+}
