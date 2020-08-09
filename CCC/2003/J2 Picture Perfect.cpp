b0ebd8a3befd231c4753619d15144f0b83009f34
Sam Liu <liu.sam2020@gmail.com>
2020-08-09 09:45:03 -0400

    changes to picture perfect

--- CCC/2003/J2 Picture Perfect.cpp
+++ CCC/2003/J2 Picture Perfect.cpp
@@ -1,11 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main() {
-    int num;
-    cin >> num;
-    // lines and lines of code
-    cout << "Minimum perimeter is " << perimeter << "with dimensions" << dimentions << " x " dimentions << endl;
+    int c, length, width;
 
-    // how to we get the dimentions and the formula for the perimeter
+    while (cin >> c) {
+        if (c == 0)
+            break;
+        length = sqrt(c);
+        width = c/length;
+
+        while (length * width != c) {
+            length = length - 1;
+            width = c/length;
+        }
+        cout << "Minimum perimeter is " << length * width << " with dimensions " << length << ' x ' << width << endl;
+    }
+        
+
     return 0;
 }
