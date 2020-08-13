daebbb361d7dd6dedf162c9766eebe799dc30c77
Sam Liu <liu.sam2020@gmail.com>
2020-08-13 13:10:50 -0400

    change

---
+++ CCC/2020 Homework.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int num;
+    
+    cin >> num;
+
+    if (num > 100) {
+        cout << "Please Enter Number Equal To Or Smaller than 100" << endl;
+    } else if (num <= 100) 
+        
+    return 0;
+}
--- Fundamentals/3.4 WhileLoop.cpp
+++ Fundamentals/3.4 WhileLoop.cpp
@@ -7,13 +7,44 @@ int main() {
     //     condition = false; 
     // }
 
-    int x = 0;
-    while (x < 10) {    
-       cout << "x is smaller than 10" << endl;
-       cout << x << endl;
 
-       x++;
-    }
+    // int x = 0;
+    // while (x < 10) {    
+    //    cout << "x is smaller than 10" << endl;
+    //    cout << x << endl;
+
+    //    x++;
+    // }
+    
+
+
+    // char input;
+    // while (cin >> input) {
+    //     if (input == 'Q') 
+    //         break;
+
+    //     cout << "Within While Loop" << endl;
+    // }
+    
+
+
+    // string input;
+    // while (cin >> input) {
+    //     if (input == "Cancel") {
+    //         break;
+    //     }
+    //     cout << input << endl;
+        
+    // }
+
+    string response;
+    do {
+        cout << "Enter menu choice " << endl;
+        cout << "More" << endl << "Quit" << endl;
+        cin >> response;
+        cout << "You Input: " << response << endl;    
+    } while (response != "Quit") ;
+    
     
     return 0;
 }
