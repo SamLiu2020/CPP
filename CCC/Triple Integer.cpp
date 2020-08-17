209b0701ac7f8a19758317c43d843826e8135a49
Sam Liu <liu.sam2020@gmail.com>
2020-08-17 11:27:39 -0400

    small change

--- CCC/Triple Integer.cpp
+++ CCC/Triple Integer.cpp
@@ -16,11 +16,12 @@ int main() {
                                 cout << i << ' ' << j << ' ' << k << endl;
         } while (num > 100);
             
-    } else if (num <= 100) 
+    } else if (num <= 100) {
         for (int i = 1; i < num; i++)
             for (int j = i+1; j < num; j++)
                 for (int k = j+1; k < num; k++)
                     if (i+j+k == num)
                         cout << i << ' ' << j << ' ' << k << endl;
+    }
     return 0;
 }
