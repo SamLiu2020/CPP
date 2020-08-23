d2a9fa05e82ab57fe64f4c7b786a86d5e53415cd
Sam Liu <liu.sam2020@gmail.com>
2020-08-23 09:32:32 -0400

    I promise, last change

--- CCC/2020/J1 Dog Treats.cpp
+++ CCC/2020/J1 Dog Treats.cpp
@@ -3,12 +3,12 @@ using namespace std;
 int main() {
     int small, medium, large;
     cin >> small >> medium >> large;
-
-    if ((1*small) + (2*medium) + (3*large) < 10 ) {
-        cout << "sad" << endl; 
-    } else if ((1*small) + (2*medium) + (3*large) >= 10 ) {
+    
+    if ((1*small) + (2*medium) + (3*large) >= 10 ) {
         cout << "happy" << endl;
-    } 
+    } else {
+        cout << "sad" << endl; 
+    }
     
     return 0;
 }
