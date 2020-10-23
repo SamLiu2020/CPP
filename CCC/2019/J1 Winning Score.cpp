dfdc25dd9e64b428dc941a4c246a04fd39378b83
Sam Liu <liu.sam2020@gmail.com>
2020-10-23 16:11:07 -0400

    change

--- CCC/2019/J1 Winning Score.cpp
+++ CCC/2019/J1 Winning Score.cpp
@@ -3,8 +3,7 @@ using namespace std;
 int main() {
     int apples3, apples2, apples1;
     int bananas3, bananas2, bananas1;
-    int applestotal, bananastotal;
-
+    
     cin >> apples3;
     cin >> apples2;
     cin >> apples1;
@@ -15,12 +14,15 @@ int main() {
     int applestotal = apples3*3+apples2*2+apples1*1;
     int bananastotal = bananas3*3+bananas2*2+bananas1*1;
 
-    if (applestotal = bananastotal){
-        cout << "T" << endl;
-    } else if (applestotal > bananastotal){
+    
+    if (applestotal > bananastotal){
         cout << "A" << endl;
-    } else if (bananastotal > applestotal){
+    }
+    if (bananastotal > applestotal){
         cout << "B" << endl;
+    }
+    if (applestotal == bananastotal){
+        cout << "T" << endl;
     }
 
     return 0;
