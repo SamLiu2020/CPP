79d349e4d419d36168d209032f11622af31451d0
SamLiu2020 <liu.sam2020@gmail.com>
2020-12-20 11:34:05 -0500

    Up and Down

--- CCC/2010/J2 Up and Down, if you have time to do.cpp
+++ CCC/2010/J2 Up and Down, if you have time to do.cpp
@@ -2,33 +2,31 @@
 using namespace std;
 int main() {
     int nikky_f, nikky_b, byron_f, byron_b, steps;
-    cin >> nikky_f, nikky_b, byron_f, byron_b, steps;
+    cin >> nikky_f >> nikky_b >> byron_f >> byron_b >> steps;
 
     int nikky_cycle = steps/(nikky_f + nikky_b);
     int Nikky_steps_taken = nikky_cycle * (nikky_f - nikky_b);
     int nikky_leftover = steps % (nikky_f + nikky_b);
     if(nikky_leftover <= nikky_f){
-        Nikky_steps_taken = Nikky_steps_taken + nikky_leftover;
+        Nikky_steps_taken += nikky_leftover;
     }else{
-        Nikky_steps_taken = (Nikky_steps_taken - nikky_f);
+        Nikky_steps_taken += (nikky_leftover - nikky_f);
     }
 
     int Byron_cycle = steps/(byron_f + byron_b);
     int Byron_steps_taken = Byron_cycle * (byron_f - byron_b);
     int byron_leftover = steps % (byron_f + byron_b);
     if(byron_leftover <= byron_f){
-        Byron_steps_taken = Byron_steps_taken + byron_leftover;
+        Byron_steps_taken += byron_leftover;
     }else{
-        Byron_steps_taken = (Byron_steps_taken - byron_f);
+        Byron_steps_taken += (byron_leftover - byron_f);
     }
 
     if (Nikky_steps_taken > Byron_steps_taken){
         cout << "Nikky" << endl;
-    }
-    if (Byron_steps_taken > Nikky_steps_taken){
+    } else if (Byron_steps_taken > Nikky_steps_taken){
         cout << "Byron" << endl;
-    }
-     if ((Byron_steps_taken = Nikky_steps_taken)){
+    } else if ((Byron_steps_taken == Nikky_steps_taken)){
         cout << "Tied" << endl;
     }
 
