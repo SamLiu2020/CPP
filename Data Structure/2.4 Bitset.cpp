863184225f3e1089fa2695721495df154410b3c5
SamLiu2020 <liu.sam2020@gmail.com>
2021-01-17 10:57:51 -0500

    Small changes

--- Data Structure/2.4 Bitset.cpp
+++ Data Structure/2.4 Bitset.cpp
@@ -1,32 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main() {
-    // bitset <8> bits_1 ("00000000");
-    // cout << bits_1 << endl;
+    // bitset <48> bits_1 ("0000000000000001");
+    // cout << sizeof(bits_1) << endl;
     // bitset <8> bits_2 (2);
     
     // biset <8> bits("11111111")
     
-    bitset <8> bits("00000000");
-    cout << "bits: " << bits << endl; 
+    // bitset <8> bits("00000000");
+    // cout << "bits: " << bits << endl; 
 
-    bits.set(7, 1); // 0000 0000 -> 0000 0001
-    cout << "bits: " << bits << endl;
+    // bits.set(7, 1); // 0000 0000 -> 0000 0001
+    // cout << "bits: " << bits << endl;
 
-    bits.reset(0); //0000 0000 -> 0000 0000
+    // bits.reset(0); //0000 0000 -> 0000 0000
 
-    int x = 5;
-    cout << bitset<8>(x) << endl;
-    string binary_str = bitset<8>(x).to_string();
-    cout << "bits string: " << binary_str << endl;
+    // int x = 5;
+    // cout << bitset<8>(x) << endl;
+    // string binary_str = bitset<8>(x).to_string();
+    // cout << "bits string: " << binary_str << endl;
     
     
-    bitset <8> bits_1 ("00000001");
-    bitset <8> bits_2 ("10000001");
-    bitset <8? bits_result;
-    bits_result = bits_1 & bits_2;
-    cout << "bits_result: " << bits_result << endl;
-    bits_result = bits_1 | bits_2;
-    cout << "bits_result: " << bits_result << endl;
+    // bitset <8> bits_1 ("00000001");
+    // bitset <8> bits_2 ("10000001");
+    // bitset <8? bits_result;
+    // bits_result = bits_1 & bits_2;
+    // cout << "bits_result: " << bits_result << endl;
+    // bits_result = bits_1 | bits_2;
+    // cout << "bits_result: " << bits_result << endl;
     return 0;
 }
