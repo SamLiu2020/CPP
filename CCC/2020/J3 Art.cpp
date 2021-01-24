6de0a1af4b5274440cf65fbf837523f1b7e2bef9
SamLiu2020 <liu.sam2020@gmail.com>
2021-01-24 11:01:50 -0500

    Small changes to J3 Art.cpp (Not done)

--- CCC/2020/J3 Art.cpp
+++ CCC/2020/J3 Art.cpp
@@ -1,15 +1,36 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main() {
+    //input
     int n, x, y;
-    cin >> n;   
-    for(int i = 0; i<n; ++i) {
-        cin >> x >> y;
-    } 
-    min_x = min x-1;
-    min_y = min y-1;
-    max_x = max x+1;
-    max_y = max y+1;
+    cin >> n;
+    char comma;
+    for(int i = 0; i<n; i++) {
+        cin >> x >> comma >> y;
+        //Calculating
+        int maximum = -INFINITY;
+        if (x > maximum){
+            maximum = x;
+        }
+        int maximum2 = -INFINITY;
+        if (y > maximum2){
+            maximum2 = y;
+        }
+        int minimum = -INFINITY;
+        if (x < minimum){
+            minimum = x;
+        }
+        int minimum2 = -INFINITY;
+        if (y < minimum2){
+            minimum2 = y;
+        }
+    }     
+    int max_x = x+1;
+    int max_y = y+1;
+    int min_x = x-1;
+    int min_y = y-1;
+
+    //Result
     cout << min_x << "," << min_y << endl;
     cout << max_x << "," << max_y << endl;   
 }
