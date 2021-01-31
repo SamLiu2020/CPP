93a69eedf1a86c3fc141f7ee303c6d1933ff3051
SamLiu2020 <liu.sam2020@gmail.com>
2021-01-31 11:28:41 -0500

    J3 Art.cpp

--- CCC/2020/J3 Art.cpp
+++ CCC/2020/J3 Art.cpp
@@ -4,31 +4,29 @@ int main() {
     int n, x, y;
     cin >> n;
     char comma;
-
+    int maximum_x = -INFINITY;
+    int maximum_y = -INFINITY;
+    int minimum_x = INFINITY;
+    int minimum_y = INFINITY;
     for(int i = 0; i<n; i++) {
         cin >> x >> comma >> y;
-    }     
-    int maximum = -INFINITY;
-    if (x > maximum){
-        maximum = x;
+        if (x > maximum_x){
+            maximum_x = x;
+        }
+        if (y > maximum_y){
+            maximum_y = y;
+        }
+        if (x < minimum_x){
+            minimum_x = x;
+        }
+        if (y < minimum_y){
+            minimum_y = y;
+        }
     }
-    int maximum2 = -INFINITY;
-    if (y > maximum2){
-        maximum2 = y;
-    }
-    int minimum = -INFINITY;
-    if (x < minimum){
-        minimum = x;
-    }
-    int minimum2 = -INFINITY;
-    if (y < minimum2){
-        minimum2 = y;
-    }
-
-    int max_x = x+1;
-    int max_y = y+1;
-    int min_x = x-1; 
-    int min_y = y-1;
+    int max_x = maximum_x+1;
+    int max_y = maximum_y+1;
+    int min_x = minimum_x-1; 
+    int min_y = minimum_y-1;
     cout << min_x << "," << min_y << endl;
     cout << max_x << "," << max_y << endl;   
 }
