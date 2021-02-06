178b2a127d2cf2dbc633a47b9e5eb59d074599c3
SamLiu2020 <liu.sam2020@gmail.com>
2021-02-06 11:27:42 -0500

    Occupy Parking changes

--- CCC/2018/J2 Occupy Parking.cpp
+++ CCC/2018/J2 Occupy Parking.cpp
@@ -6,11 +6,10 @@ int main() {
     int number_occupied = 0;
     cin >> number_of_parking_lots >> yesterday >> today;
 
-    for(int i = 0; i <= number_of_parking_lots; i++){
+    for(int i = 0; i < number_of_parking_lots; i++){
         if(today[i] == 'C' && yesterday[i] == 'C')
             number_occupied++;
     }
 
     cout << number_occupied << endl;
-    return 0;
 }
