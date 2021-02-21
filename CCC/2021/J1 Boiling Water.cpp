b2b6f1221aa20f902f46cb1933546cec28c63531
SamLiu2020 <liu.sam2020@gmail.com>
2021-02-21 09:16:56 -0500

    2021 J1

---
+++ CCC/2021/J1 Boiling Water.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int b;
+    cin >> b;
+    int p = 5*b-400;
+    cout << p << endl; 
+    if(p == 100){
+        cout << "0" << endl;
+    }
+    if(p > 100){
+        cout << "-1" << endl;
+    }
+    if(p < 100){
+        cout << "1" << endl;
+    } 
+    
+    return 0;
+}
