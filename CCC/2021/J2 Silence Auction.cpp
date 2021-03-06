1bedaf68b267f6ca7fd906bb2d541eb5a4af62f8
SamLiu2020 <liu.sam2020@gmail.com>
2021-03-06 10:16:58 -0500

    J2 Silence Auction

--- CCC/2021/J2 Silence Auction.cpp
+++ CCC/2021/J2 Silence Auction.cpp
@@ -1,17 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main() {
-   int n; 
-   cin >> n;
-   string winner, name;
-   int bid, bid2;
-   for (int i = 0; i < n; i++){
+    int n; 
+    cin >> n;
+    string winner, name;
+    int bid, bid2;
+    for (int i = 0; i < n; i++){
         cin >> name;
         cin >> bid;
         if (bid > bid2) {
             bid2 = bid;
             winner = name;
         }
-   }
-   cout << winner << endl;
+    }
+    cout << winner << endl;
 }
