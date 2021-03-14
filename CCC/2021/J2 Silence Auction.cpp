7b09e47e8dd9f4c55954c4faad61a2ad3110c9af
SamLiu2020 <liu.sam2020@gmail.com>
2021-03-14 09:39:54 -0400

    Some changes

--- CCC/2021/J2 Silence Auction.cpp
+++ CCC/2021/J2 Silence Auction.cpp
@@ -1,10 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main() {
+void using_map() {
+    int n, bids, bids_2 = 0;
+    cin >> n;
+    string name, winner;
+    map <string, int> my_map;
+    for (int i = 0; i < n; i++){
+        cin >> name;
+        cin >> bids;
+        my_map[name] = bids;
+        if (my_map[name] > bids_2) {
+            bids_2 = my_map[name];
+            winner = name;
+        }
+    }
+    cout << winner << endl;
+}
+void not_using_map() {
     int n; 
     cin >> n;
     string winner, name;
-    int bid, bid2;
+    int bid, bid2 = 0;
     for (int i = 0; i < n; i++){
         cin >> name;
         cin >> bid;
@@ -14,4 +30,10 @@ int main() {
         }
     }
     cout << winner << endl;
+}
+
+int main() {
+    using_map();
+    not_using_map();
+    // Two solutions: Using map and not using map.
 }
