d79e10cae6e6ed64e28f7ddd633f0cef56e11834
SamLiu2020 <liu.sam2020@gmail.com>
2021-03-28 09:43:35 -0400

    J3 Returning Home

---
+++ CCC/2005/J3 Returning Home.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    stack <string> stk_direction;
+    stack <string> stk_street;
+    string left_or_right, my_street;
+
+    while (my_street != "SCHOOL"){
+        cin >> left_or_right >> my_street;
+        stk_direction.push(left_or_right);
+        stk_street.push(my_street);
+    }
+    while (!stk_street.empty()){
+        stk_street.pop();
+        if((stk_direction.top() == "R") && (stk_direction.size() != 1)){
+            cout << "Turn LEFT onto " << stk_street.top() << " street." << endl;
+        }
+        if((stk_direction.top() == "L") && (stk_direction.size() != 1)){
+            cout << "Turn RIGHT onto " << stk_street.top() << " street." << endl;
+        }
+        if((stk_direction.top() == "R") && (stk_direction.size() == 1)){
+            cout << "Turn LEFT into your HOME." << endl;
+        }
+        if((stk_direction.top() == "L") && (stk_direction.size() == 1)){
+            cout << "Turn RIGHT into your HOME." << endl;
+        }
+        stk_direction.pop();
+    }
+    
+    return 0;
+}
