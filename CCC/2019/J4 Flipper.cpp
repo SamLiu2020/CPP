0ced5d03873118600ded3d9603ea4f06ced0c3e2
SamLiu2020 <liu.sam2020@gmail.com>
2021-04-15 17:11:41 -0400

    NOT DONE JUST SAVING IT

---
+++ CCC/2019/J4 Flipper.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    string input;
+    int original_grid[2][2] = {
+        {1, 2},
+        {3, 4}
+    };
+    cin >> input;
+
+    while (input != endl){
+        if (input == "H"){
+            swap(original_grid[0], original_grid[1]);
+        } 
+        if (input == "V"){
+            swap(original_grid[0][0], original_grid[0][1]);
+            swap(original_grid[1][0], original_grid[1][1]);
+        }
+    }
+
+    cout << original_grid[0][0] << ' ' << original_grid[0][1] << endl;
+    cout << original_grid[1][0] << ' ' << original_grid[1][1] << endl;
+
+    return 0;
+}
