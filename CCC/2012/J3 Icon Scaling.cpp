224488d42e4f3ae73f9092ba04741a74523dc3ec
SamLiu2020 <liu.sam2020@gmail.com>
2021-04-16 18:27:27 -0400

    Icon Scaling (Final Changes)

--- CCC/2012/J3 Icon Scaling.cpp
+++ CCC/2012/J3 Icon Scaling.cpp
@@ -1,62 +1,60 @@
 #include <bits/stdc++.h>
 using namespace std;
-// void func() {
-//     string group1, group2, group3;
-//     int k;
-//     cin >> k;
+void func1() {
+    string group1, group2, group3;
+    int k;
+    cin >> k;
 
-//     for (int i = 0; i < k; i++) {
-//         group1 += "*";
-//         group2 += " ";
-//         group3 += "*";
-//     }
-//     for (int i = 0; i < k; i++) {
-//         group1 += "x";
-//         group2 += "x";
-//         group3 += " ";
-//     }
-//     for (int i = 0; i < k; i++) {
-//         group1 += "*";
-//         group2 += "x";
-//         group3 += "*";
-//     }
-//     for (int i = 0; i < k; i++) {
-//         cout << group1 << endl;
-//     }
-//     for (int i = 0; i < k; i++) {
-//         cout << group2 << endl;
-//     }
-//     for (int i = 0; i < k; i++) {
-//         cout << group3 << endl;
-//     }
-// }
-// int main() {
-//     func();
-//     return 0;
-// }
+    for (int i = 0; i < k; i++) {
+        group1 += "*";
+        group2 += " ";
+        group3 += "*";
+    }
+    for (int i = 0; i < k; i++) {
+        group1 += "x";
+        group2 += "x";
+        group3 += " ";
+    }
+    for (int i = 0; i < k; i++) {
+        group1 += "*";
+        group2 += "x";
+        group3 += "*";
+    }
+    for (int i = 0; i < k; i++) {
+        cout << group1 << endl;
+    }
+    for (int i = 0; i < k; i++) {
+        cout << group2 << endl;
+    }
+    for (int i = 0; i < k; i++) {
+        cout << group3 << endl;
+    }
+}
 
-void func() {
+void func2() {
     int k;
     cin >> k;
 
     char arr_2d[3][3] = {
-        {"*","x","*"},
-        {" ","x","x"},
-        {"*"," ","*"}
+        {'*','x','*'},
+        {' ','x','x'},
+        {'*',' ','*'}
     };
 
     for (int rows = 0; rows < 3; rows++){
         for (int i = 0; i < k; i++) {
             for (int column = 0; column < 3; column++){
                 for (int j = 0; j < k; j++){
-                    cout << arr_2d[rows][column] << endl;
+                    cout << arr_2d[rows][column];
                 }
             }
         cout << endl;               
         }
     }
 } 
+
 int main() {
-    func();
+    func1();
+    func2();
     return 0;
 }
