62efedd27179d10b46b61509a3d4af10f72f084d
SamLiu2020 <liu.sam2020@gmail.com>
2021-04-23 19:15:22 -0400

    Flipper done yay!

--- CCC/2019/J4 Flipper.cpp
+++ CCC/2019/J4 Flipper.cpp
@@ -2,20 +2,22 @@
 using namespace std;
 int main() {
     string input;
+    cin >> input;
     int original_grid[2][2] = {
         {1, 2},
         {3, 4}
     };
-    cin >> input;
 
-    if (input == "H"){
-        swap(original_grid[0][0], original_grid[0][1]);
-        swap(original_grid[1][0], original_grid[1][1]);
+    for (int i = 0; i < int(input.size()); i++){
+        if (input[i] == 'V'){
+            swap(original_grid[0][0], original_grid[0][1]);
+            swap(original_grid[1][0], original_grid[1][1]);
+        }  
+        if (input[i] == 'H'){
+            swap(original_grid[0][0], original_grid[1][0]);
+            swap(original_grid[0][1], original_grid[1][1]);
+        }
     } 
-    if (input == "V"){
-        swap(original_grid[0][0], original_grid[1][0]);
-        swap(original_grid[0][1], original_grid[1][1]);
-    }
 
     cout << original_grid[0][0] << ' ' << original_grid[0][1] << endl;
     cout << original_grid[1][0] << ' ' << original_grid[1][1] << endl;
