20acda138529be67de0ce09e2be9ae57506a872d
SamLiu2020 <liu.sam2020@gmail.com>
2021-04-30 19:52:49 -0400

    Changes

--- Data Structure/2.1 Array.cpp
+++ Data Structure/2.1 Array.cpp
@@ -4,12 +4,17 @@ using namespace std;
 void initialize_array(){
     int array_0[5];       // It may has random values in the array
     int array_1[5] = {};  // Initialize all element to 0 with {}
-    int array_2[5] = {1, 1, 1, 1, 1}; // GIve initial value
+    int array_2[5] = {1, 1, 1, 1, 1}; // Give initial value
 }
-int main(){
+void access_value(){
     int something[3] = {10, 20, 30};
     cout << something[0] << endl;
     cout << something[1] << endl;
     cout << something[2];
+}
+
+int main(){
+    initialize_array();
+    access_value();
     return 0;
 }
---
+++ Data Structure/2.2 Char Array.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void declare_string(){
+    string greeting = "Hello World!";
+    cout << greeting << endl;
+
+    string str = "Hello world!";
+    for (int i = 0; i < 12; i++){
+        cout << str[i];
+    }
+    cout << endl;
+}
+
+int main(){
+    declare_string();
+    return 0;
+}
