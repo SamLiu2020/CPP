a78d45908759f61fb584e5a7b4a566125f68cc88
SamLiu2020 <liu.sam2020@gmail.com>
2021-05-28 19:00:51 -0400

    Vector update

--- Data Structure/2.3 Vector.cpp
+++ Data Structure/2.3 Vector.cpp
@@ -1,20 +1,45 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main() {
-    // vector <int> vect = {1, 2, 3, 4}; 
+void What_is_Vector(){
+    Vectors are sequence containers representing arrays that can change in size. (Dynamic size array)
+    int array[4] = {1, 2, 3, 4};
+    vector <int> vect = {1, 2, 3, 4};
+        (Data Type)
+}
+void Access_and_Iterate_Vector(){
+    cout << vect[0] << endl;
 
-    // cout << vect[0] << endl;
+    for (int i = 0; i < vect.size(); i++) {
+        cout << vect[i] << endl;
+    }
 
-    // for (int i = 0; i < vect.size(); i++) {
-    //     cout << vect[i] << endl;
-    // }
+    for (auto item : vect) {
+        cout << item << endl;
+    }
+}
+void TwoD_Vector(){
+    vector <vector <int>> vect{
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9}
+    };
+    // Displaying the 2D vector
+    for (int i = 0; i < vect.size(); i++){
+        for (int j = 0; j < vect[i].size(); i++)
+            cout << vect[i][j] << " ";
+        cout << endl;
+    }
+}
+void Commonly_Used_Vector_Operations(){
+    vect.push_back(5); // Add element to the end;
+    // vect.push_back("element"); // Add element to the end;
+    vect.insert(vect.begin()+1, 10); // Insert element by index, insert "10" to second element
 
-    // for (auto item : vect) {
-    //     cout << item << endl;
-    // }
-
-
-
+    vect.pop_back(); // Remove the last element
+    vect.erase(vect.begin()); // Remove the first element
+    vect.erase(vect.begin()+1, vect.begin()+3); // Remove the second element to third element
+}
+void Vector_Iterator(){
     vector <int> vect = {5, 4, 3, 1, 6, 2};
 
     vect.front(); // Value
@@ -25,17 +50,11 @@ int main() {
     sort(vect.begin(), vect.end()); // Ascending
     sort(vect.begin(), vect.end(), greater<>()); // Descending
     sort(vect.begin(), vect.begin() + 3);
+}
 
-    vect.push_back(5); // Add element to the end;
-    // vect.push_back("element"); // Add element to the end;
-    vect.insert(vect.begin()+1, 10); // Insert element by index, insert "10" to second element
-
-    vect.pop_back(); // Remove the last element
-    vect.erase(vect.begin()); // Remove the first element
-    vect.erase(vect.begin()+1, vect.begin()+3); // Remove the second element to third element
-
-    for (auto item : vect) {
-        cout << item << endl;
-    }
- 
+int main() {
+    Access_and_Iterate_Vector();
+    TwoD_Vector();
+    Commonly_Used_Vector_Operations();
+    Vector_Iterator();
 }
