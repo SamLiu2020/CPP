d360a2944b98e10c5f63b7c36687d7388e6b6a32
SamLiu2020 <liu.sam2020@gmail.com>
2021-06-04 19:39:34 -0400

    changes

--- CCC/2020/J4 Cyclic Shifts.cpp
+++ CCC/2020/J4 Cyclic Shifts.cpp
@@ -4,13 +4,17 @@ int main() {
     string t, s;
     cin >> t >> s;
     for (int i = 0; i < s.size(); i++){
-        s.push_back(s[0]);
-        s.erase(s.begin()); 
-        size_t found = t.find(s);
-        if (found == s.size()){
-            cout << "no" << endl;
+        if (t.find(s)){
+            cout << "yes" << endl;
             break;
         } 
+        if (!t.find(s)) {
+            s.push_back(s[0]);
+            s.erase(s.begin()); 
+        }
     }
-    cout << "yes" << endl;
+    if (!t.find(s)) {
+        cout << "no" << endl;
+    }
+    
 }
