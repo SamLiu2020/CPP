5841049737ee73821fb18a083c4f7e7fee5405b1
SamLiu2020 <liu.sam2020@gmail.com>
2021-06-11 18:35:20 -0400

    FINISHED

--- CCC/2020/J4 Cyclic Shifts.cpp
+++ CCC/2020/J4 Cyclic Shifts.cpp
@@ -3,18 +3,15 @@ using namespace std;
 int main() {
     string t, s;
     cin >> t >> s;
-    for (int i = 0; i < s.size(); i++){
-        if (t.find(s)){
+    for (int i = 0; i < int(s.size()); i++){
+        if (t.find(s) != string::npos){     // t.find(s) is return the index(int) where is the substring
             cout << "yes" << endl;
             break;
-        } 
-        if (!t.find(s)) {
+        } else {
             s.push_back(s[0]);
             s.erase(s.begin()); 
         }
     }
-    if (!t.find(s)) {
+    if (t.find(s) == string::npos)
         cout << "no" << endl;
-    }
-    
 }
