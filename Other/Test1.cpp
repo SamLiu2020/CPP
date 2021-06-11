d7737022865d4651a1523e293180f87c78b2c357
SamLiu2020 <liu.sam2020@gmail.com>
2021-06-11 19:44:13 -0400

    small changes

--- Other/Test1.cpp
+++ Other/Test1.cpp
@@ -1,14 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main() {
-    // int arr[6] = {4, 45, 31, 45, 2, 12};
-    // int sum = 0;
-    // for (int i = 0; i < 6; i++)
-    //     sum += arr[i];
-    // cout << sum << endl;
-
-    if (0) {
-        cout << "It is true!" << endl;
-    }
-    return 0;
+    string str = "Hello World!";
+    auto found_idx = str.find("Wo");
+    
+    if (found_idx != string::npos)
+        cout << "Found at: " << found_idx << endl;
+    else
+        cout << "Not found!" << endl;
 }
